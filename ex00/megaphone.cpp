8b7f09b395595bb8ce5d1e4b5eeedd46dbee8b59
Taeil-Nam <67255013+Taeil-Nam@users.noreply.github.com>
2023-06-13 22:17:31 +0900

    add: ex00 완료

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	size_t i = 1;
+	size_t j = 0;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if ('a' <= argv[i][j] && argv[i][j] <= 'z')
+				argv[i][j] -= 32;
+			j++;
+		}
+		std::cout << argv[i];
+		i++;
+	}
+	std::cout << std::endl;
 
 	return 0;
 }
